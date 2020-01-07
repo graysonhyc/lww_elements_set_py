3b87b1149421ae4ad768e8d6bc3f8ccca1b0e209
Grayson Ho <graysonhyc@gmail.com>
2020-01-07 12:26:00 +0800

    Added Documentation

--- lww_python.py
+++ lww_python.py
@@ -1,44 +1,125 @@
 from threading import Semaphore
 from collections import defaultdict
 
-class lww_element_set():
+class LWW_ElementSet():
+    """A Python implementation of Last-Writer-Win (LWW) element set.
+
+    The LWW element set stores a single instance of each element, coupled with 
+    the timestamp of insertion, i.e., {lww element: timestamp}. 
+    """
+
     def __init__(self):
+        """LWW element set constructor
+
+        Points to note:
+        -Python dictionaries are used as the main data structure.
+        -Insertion, Update and Searching can be done in O(1) time.
+        -The data type of timestamp is a floating point number.
+        -Semaphores are added to ensure thread-safe operations.
+        """
         self.add_set = defaultdict(float)
         self.remove_set = defaultdict(float)
         self.add_sem = Semaphore()
         self.remove_sem = Semaphore()
 
     def add(self, element, timestamp):
+        """Add an element to the LWW element set, or update its timestamp if it already exists.
+        
+        Elements are added to an LWW-Element-Set by inserting the element 
+        into the add set, with a timestamp.
+
+        The timestamp input arguemnt is first converted to float. Then, this method
+        tries to add the element with timestamp into the add_set. 
+
+        Returns True if insertion is successful, or False if any exception occur.
+        """
         timestamp = float(timestamp)
+        success = True
         self.add_sem.acquire()
-        self.check_and_insert(self.add_set, element, timestamp)
-        self.add_sem.release()
+        try: 
+            self.check_and_insert(self.add_set, element, timestamp)
+        except:
+            success = False 
+        finally:
+            self.add_sem.release() #unlock no matter what
+        return success
 
     def remove(self, element, timestamp):
+        """Remove an element from the LWW element set.
+        
+        Elements are removed from the LWW-Element-Set by being added 
+        to the remove set, again with a timestamp.
+
+        Note that there is a conceptual difference between LWW set removal and ordinary 
+        set removal. In LWW set operation, a removal does not actually remove an element 
+        from the set, but to add a "removal record" in the remove set.
+
+        The timestamp input arguemnt is first converted to float. Then, this method
+        tries to add the element with timestamp into the remove_set. 
+
+        Returns True if insertion is successful, or False if any exception occur.
+        """
         timestamp = float(timestamp)
+        success = True
         self.remove_sem.acquire()
-        self.check_and_insert(self.remove_set, element, timestamp)
-        self.remove_sem.release()
+        try:
+            self.check_and_insert(self.remove_set, element, timestamp)
+        except:
+            success = False
+        finally:
+            self.remove_sem.release() #unlock no matter what
+        return success
 
     def check_and_insert(self, target_set, element, timestamp):
-        if element in target_set:
-            curr_timestamp = target_set[element]
-            if curr_timestamp < timestamp:
-                target_set[element] = timestamp
-        else:
-            target_set[element] = timestamp
+        """A helper function to add element/update timestamp in the target set.
+
+        This helper function adds an element in the target set if an element 
+        does not exist, or update the original timestamp if the input timestamp 
+        is newer.
+
+        The use of defaultdict automatically assigns the return value to 0.0
+        if element key does not exist. Since timestamp is >= 0, we can use the following
+        one-liner.
+
+        Input arguments:
+        - target_set: add_set/remove_set 
+        - element: the element to be added
+        - timestamp: the time of insertion (float, non-negative)
+        """
+        target_set[element] = max(target_set[element], timestamp)
 
     def exist(self, element):
+        """Determine if an element exists in the LWW element set.
+
+        An element is a member of the LWW-Element-Set if it is in the add set, 
+        and either not in the remove set, or in the remove set but with an earlier 
+        timestamp than the latest timestamp in the add set. We bias "add" if the "add" and
+        "remove" of the same element has the same timestamp to prevent data loss.
+
+        Returns True if element exists, or False if element doesn't exist.
+        """
+
+        # The element was never added
         if element not in self.add_set:
             return False
+        # The element was added before and was never removed
         elif element not in self.remove_set:
             return True
+        # The element was added and removed before, but the timestamp of "add" is 
+        # larger than that of "remove"
         elif self.add_set[element] >= self.remove_set[element]:
             return True
+        # The element was added and removed before, but the timstamp of "remove" is 
+        # larger than that of "add"
         else:
             return False
 
     def get(self):
+        """Return a list of all existing elements in the LWW element set
+
+        This method makes use of exist() and checks all elements in the add_set.
+        Elements will be copied to the result list if they are not removed eventually.
+        """
         result = []
         for element in self.add_set:
             if self.exist(element):
--- lww_tests_python.py
+++ lww_tests_python.py
@@ -1,16 +1,26 @@
 import unittest
-from lww_python import lww_element_set
+from lww_python import LWW_ElementSet
 
-# Associativity, Commutativity, Idempotence
-class test_lww_element_set(unittest.TestCase):
+class Test_LWW_ElementSet(unittest.TestCase):
+    # Test for Idempotent property
     def test1(self):
+        lww = LWW_ElementSet()
+        lww.add(1,1)
+        lww.add(1,1)
+        self.assertTrue(lww.exist(1))
+        lww.add(1,0)
+        expected_res = [1]
+        self.assertEqual(lww.get(), expected_res)
+
+    # Test for Communtative property
+    def test2(self):
         pass
 
-    def test2(self):
-        pass 
-
+    # Test for Associative property
     def test3(self):
         pass
+
+    # Test for thread-safety
 
 if __name__ == "__main__":
     unittest.main()
